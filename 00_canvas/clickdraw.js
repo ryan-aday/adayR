af96e3aa074c9766797c27f87a2058205c34d479
Ryan Aday <ryanobelisk@gmail.com>
2019-01-31 06:10:59 -0500

    Added clear function to js

--- 00_canvas/clickdraw.js
+++ 00_canvas/clickdraw.js
@@ -1,4 +1,11 @@
 var c = document.getElementById("slate);
 var ctx = c.getContext("2d");
-ctx.fillStyle = "#FF0000";
-ctx.fillRect(10, 10, 10, 10);
+
+//Code from class
+//ctx.fillStyle = "#FF0000";
+//ctx.fillRect(10, 10, 10, 10);
+
+//Clea Rect function
+document.getElementById("clear").addEventListener("click", function() {
+  ctx.clearRect(0, 0, c.width, c.height); 
+});
