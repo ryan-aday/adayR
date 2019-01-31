eaac9f8ddd711105a40e47b4d93e9ea93f52f4a6
Ryan Aday <ryanobelisk@gmail.com>
2019-01-31 06:03:43 -0500

    Errors woth browser not displaying red rect

---
+++ 00_canvas/clickdraw.js
@@ -0,0 +1,4 @@
+var c = document.getElementById("slate);
+var ctx = c.getContext("2d");
+ctx.fillStyle = "#FF0000";
+ctx.fillRect(10, 10, 10, 10);
---
+++ 00_canvas/templates/check.js
@@ -0,0 +1,3 @@
+var c = document.getElementById("slate");
+var ctx=c.getContext("2d");
+ctx.fillRect(50, 50 100, 200);
