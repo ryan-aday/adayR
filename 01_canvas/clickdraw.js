ee134d52da58bcfdf4bb94dfbe6eb21e040c898a
Ryan Aday <ryanobelisk@gmail.com>
2019-02-01 07:42:11 -0500

    added preventdefault()

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -1,5 +1,13 @@
+//Ryan aday
+//SoftDev2 pd8
+//K01> -- <Title/Topic/Summary>
+//2019-02-1   
+
+
 var c = document.getElementById("slate");
 var ctx = c.getContext("2d");
+
+var counter=0;
 
 //Code from class
 //ctx.fillStyle = "#FF0000";
@@ -13,29 +21,30 @@ document.getElementById("clear").addEventListener("click", function() {
 
 //toggle Button Name
 document.getElementById("toggle").addEventListener("click", function() {
-  if (document.getElementById("toggle").innerHTML =="Rect"){
-    document.getElementById("toggle").innerHTML = "Dot";
-  }
-  else
-  {
-    document.getElementById("toggle").innerHTML = "Rect";
-  }
+  counter++;
 });
 
 //Toggle Bwtn displaying button and dot
 document.getElementById("slate").addEventListener("click", function(e) {
-  if (document.getElementById("toggle").innerHTML =="Rect"){
+
+  if (counter%3==0){
     ctx.beginPath();
     ctx.rect(e.offsetX, e.offsetY, 150, 100);
     ctx.fillStyle = "red";
     ctx.fill();
+
   }
   //beginPath initiates canvas to be changed
   //offsetX and offsetY take x and y coords of cursor
-  else if(document.getElementById("toggle").innerHTML == "Dot"){
+  else if (counter%3==1){
     ctx.beginPath();
     ctx.arc(e.offsetX, e.offsetY, 5, 0, 2*Math.PI);
     ctx.fillStyle = "green";
     ctx.fill();;
+  }
+  else{
+      ctx.preventDefault();
+      //prevent dafault event from happening- in this case, the event
+      //triggered if the modulo was 0
   }
 });
