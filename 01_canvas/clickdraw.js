1e16bf6d016dbb56828b4e5f9df308a9f94e051f
Ryan Aday <ryanobelisk@gmail.com>
2019-02-01 10:56:55 -0500

    Made preventdefault more useful

--- 01_canvas/clickdraw.js
+++ 01_canvas/clickdraw.js
@@ -1,7 +1,7 @@
-//Ryan aday
+//Ryan Aday
 //SoftDev2 pd8
-//K01> -- <Title/Topic/Summary>
-//2019-02-1   
+//K01> --  ...and I want to Paint It Better
+//2019-02-01
 
 
 var c = document.getElementById("slate");
@@ -14,9 +14,23 @@ var counter=0;
 //ctx.fillRect(10, 10, 10, 10);
 
 //Clear Rect function
+function isCanvasBlank(canvas) {
+    var blank = document.createElement('canvas');
+    blank.width = canvas.width;
+    blank.height = canvas.height;
+
+    return canvas.toDataURL() == blank.toDataURL();
+}
+
 document.getElementById("clear").addEventListener("click", function() {
-  ctx.clearRect(0, 0, c.width, c.height);
-  ctx.clearArc(0, 0, c.width, c.height);
+  if (isCanvasBlank(c))
+    {
+      //prevent dafault event from happening- in this case, the event
+      //triggered if the modulo was 0
+    ctx.preventDefault();
+  }
+      ctx.clearRect(0, 0, c.width, c.height);
+      ctx.clearArc(0, 0, c.width, c.height);
 });
 
 //toggle Button Name
@@ -27,7 +41,7 @@ document.getElementById("toggle").addEventListener("click", function() {
 //Toggle Bwtn displaying button and dot
 document.getElementById("slate").addEventListener("click", function(e) {
 
-  if (counter%3==0){
+  if (counter%2==0){
     ctx.beginPath();
     ctx.rect(e.offsetX, e.offsetY, 150, 100);
     ctx.fillStyle = "red";
@@ -36,15 +50,11 @@ document.getElementById("slate").addEventListener("click", function(e) {
   }
   //beginPath initiates canvas to be changed
   //offsetX and offsetY take x and y coords of cursor
-  else if (counter%3==1){
+  else {
     ctx.beginPath();
     ctx.arc(e.offsetX, e.offsetY, 5, 0, 2*Math.PI);
     ctx.fillStyle = "green";
     ctx.fill();;
   }
-  else{
-      ctx.preventDefault();
-      //prevent dafault event from happening- in this case, the event
-      //triggered if the modulo was 0
-  }
+
 });
