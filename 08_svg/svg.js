84c0cecc958d308669b5c6044ca5d79e99467726
ryan-aday <raday@stuy.edu>
2019-03-12 14:05:34 -0400

    added new dir

---
+++ 08_svg/svg.js
@@ -0,0 +1,10 @@
+var c=document.createElementNS(
+"http://www.w3.org/2000/svg", "circle");
+
+c.setAttibute("cx",0)
+c.setAttibute("cy",0)
+c.setAttibute("cx",0)
+c.setAttibute("cx",0)
+c.setAttibute("cx",0)
+c.setAttibute("cx",0)
+c.setAttibute("cx",0)
