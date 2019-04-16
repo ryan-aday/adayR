7741af1766d23cbb103cd096717b5ca753ee5e2d
Ryan Aday <ryanobelisk@gmail.com>
2019-04-16 13:26:17 -0400

    added 8

--- 17_cw.py
+++ 17_cw.py
@@ -29,14 +29,20 @@ y=[t for t in range(1,x+1) if x%t==0]
 print("#6: "+str(y))
 
 #7
-lists=[]
-nwlst=[["A", "B", "C"]
-       ["F", "G", "H"]
-       ]
-
-
+mat=[[3,0,1],[2,1,7],[3,6,9]]
+y=[[mat[i][j] for i in range (len(mat))]
+ for j in range(len(mat))]
 print("#7: "+str(y))
 
 # Ans: [["A", "F"]["B", "G"], ["C", "H"]]
 
-y=[lists.append]
+#8
+pyth=[]
+a=7
+#b=[x for x in range(y+1) for y in range(z+1) for z in range(2, a+1) if (x**2 + y**2) == z**2]
+b=[[x,y,z] for z in range(y, a+1) for y in range(x, z+1) for x in range(y+1) if (x**2 + y**2) == z**2]
+print ("#8: "+str(b))
+#9
+data=[2, 0, -1, 3]
+y=[z for z in range(t) for t in range(len(data)) if z<=t]
+print("#8: "+str(y))
