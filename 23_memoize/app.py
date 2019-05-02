fa3325fc2f63b0e4866c3516e0dfd6a7ef45424f
Ryan Aday <ryanobelisk@gmail.com>
2019-05-02 13:42:15 -0400

    playing around with yesterday's work

---
+++ 23_memoize/app.py
@@ -0,0 +1,37 @@
+import random
+
+def make_HTML_heading(f):
+    txt=f()
+    def inner():
+            return '<h1>'+txt+'</h1>'
+    return inner
+
+make_HTML_heading
+def greet():
+    greetings=['hello', 'welcome', 'hi', 'hola']
+    return random.choice(greetings)
+
+greet_heading=make_HTML_heading(greet)
+print(greet())
+print(greet())
+print(greet())
+print(greet())
+
+def fib(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+def memoize(f):
+    memo={}
+    def helper(x):
+        if x not in memo:
+            memo[x]=f(x)
+        return memo[x]
+    return helper
+
+fib=memoize(fib)
+print(fib(40))
